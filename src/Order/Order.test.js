a84b6676de0d48f609e5b92428b0ea27987172c1
smet_k <ski97@mail.ru>
2019-10-14 12:20:43 +0300

    fix tabs and expect

--- src/Order/Order.test.js
+++ src/Order/Order.test.js
@@ -1,7 +1,7 @@
 import { shallow, configure } from 'enzyme';
 import React from 'react';
 
-import {fakeOrders} from '../mock/fakeOrders';
+import { fakeOrders } from '../mock/fakeOrders';
 import Order from './Order';
 import Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json';
@@ -9,39 +9,41 @@ import toJson from 'enzyme-to-json';
 configure({ adapter: new Adapter() });
 
 describe('Order component', () => {
-    const wrapper = shallow(<Order order={fakeOrders[0]}/>);
-    const wrapperSecond = shallow(<Order order={fakeOrders[1]}/>);
-    const wrapperNotValid = shallow(<Order order={{}}/>);
-    const wrapperNewElement = shallow(<Order order={{
-		id: -1,
-		date: 1544356800001,
-		shop: 'kek Express',
-		items: [
-			'kek'
-		]
-	}}/>);
+	it('render first element', () => {
+		const wrapper = shallow(<Order order={fakeOrders[0]} />);
 
-    it('render first element', () => {
-        expect(toJson(wrapper)).toMatchSnapshot();
-    });
+		expect(toJson(wrapper)).toMatchSnapshot();
+	});
 
-    it('render second element', () => {
-        expect(toJson(wrapperSecond)).toMatchSnapshot();
-    });
+	it('render second element', () => {
+		const wrapperSecond = shallow(<Order order={fakeOrders[1]} />);
 
-    it('render empty element', () => {
-        expect(toJson(wrapperNotValid)).toMatchSnapshot();
+		expect(toJson(wrapperSecond)).toMatchSnapshot();
+	});
 
-        expect(toJson(wrapperNotValid)).not.toEqual(toJson(wrapper))
-    });
+	it('render empty element', () => {
+		const wrapperNotValid = shallow(<Order order={{}} />);
 
-    it('render new element', () => {
-        expect(toJson(wrapperNewElement)).toMatchSnapshot();
+		expect(toJson(wrapperNotValid)).toMatchSnapshot();
+	});
 
-        expect(toJson(wrapperNewElement)).not.toEqual(toJson(wrapper))
-        expect(toJson(wrapperNewElement)).not.toEqual(toJson(wrapperSecond))
-        expect(toJson(wrapperNewElement)).not.toEqual(toJson(wrapperNotValid))
-        
-        expect(toJson(wrapperNewElement)).toEqual(toJson(wrapperNewElement))
-    });
+	it('render empty element not equal first element', () => {
+		const wrapperNotValid = shallow(<Order order={{}} />);
+		const wrapper = shallow(<Order order={fakeOrders[0]} />);
+
+		expect(toJson(wrapperNotValid)).not.toEqual(toJson(wrapper));
+	});
+
+	it('render new element', () => {
+		const wrapperNewElement = shallow(<Order order={{
+			id: -1,
+			date: 1544356800001,
+			shop: 'kek Express',
+			items: [
+				'kek'
+			]
+		}} />);
+
+		expect(toJson(wrapperNewElement)).toMatchSnapshot();
+	});
 })
